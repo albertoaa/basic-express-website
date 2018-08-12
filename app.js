dcc0956c1eca1104e19406a75a242f00db162d19
Alberto Aragon <alberto@alturasoluciones.com>
2018-08-12 09:09:31 -0500

    Page routes and views

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const nodeMailer = require('nodemailer');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send("Hello world");
+  res.render('index', {title: 'Welcome'});
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(3000);
