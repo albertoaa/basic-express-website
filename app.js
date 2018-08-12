edf352a2a928516a80e2be30a05a984a958fd0c8
Alberto Aragon <alberto@alturasoluciones.com>
2018-08-12 09:35:32 -0500

    Sending emails with Nodemailer package

--- app.js
+++ app.js
@@ -18,6 +18,36 @@ app.get('/', (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about");
 });
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.post("/contact/send", (req, res) => {
+  let transporter = nodeMailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'ronin8503@gmail.com',
+      pass: 'Nohycs12sohs1s.JMP'
+    }
+  });
+
+  let mailOptions = { 
+    from: "Alberto Aragon <ronin8503@gmail.com>", 
+    to: "aragonalvarez@gmail.com",
+    subject: "Website Submission",
+    text: "You have a submission with the following details... Name: " + req.body.name + "Email: " + req.body.email + "Message: " + req.body.message,
+    html: "<p>You have a submission with the following details...</p><ul><li>Name: " + req.body.name +"</li><li>Email: " + req.body.email +"</li><li>Message: " + req.body.message + "</li></ul>"
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if(error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message Sent: ' + info.response);
+      res.redirect('/');
+    }
+  })
+});
 
 app.listen(3000);
 console.log('Server is runing on port 3000');
